be3d01123ba307243761807a96b64d8fabdbf9e6
unknown <yakovlev.drezna.stydy@mail.ru>
2023-02-26 16:15:31 +0300

    this

--- this.js
+++ this.js
@@ -1,9 +1,18 @@
-let user = {
-  name: "Petro",
-  age: 30,
-};
+// let user = {
+//   name: "Petro",
+//   age: 30,
+// };
 
-user.sayHi = function () {
-  console.log("Hello!");
+// user.sayHi = function () {
+//   console.log("Hello!");
+// };
+// user.sayHi();
+
+let user = {
+  name: "Jorik",
+  age: 15,
+  sayHi() {
+    console.log(this.age);
+  },
 };
 user.sayHi();
